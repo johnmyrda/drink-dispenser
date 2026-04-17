daf3bce42b3abbfe37f8d6b90b483fde23659a83
John Myrda <hello@johnmyrda.com>
2026-04-17 09:08:15 -0500

    Making progress on class primitives

--- drink_dispenser.py
+++ drink_dispenser.py
@@ -1,6 +1,9 @@
+import atexit
 import subprocess
+from dataclasses import dataclass
+from itertools import count
 
-from gpiozero import LED, PWMLED, Button
+from gpiozero import PWMLED, Button, DigitalOutputDevice
 import configparser
 
 
@@ -12,33 +15,94 @@ class DrinkButton(Button):
         else: 
             return "o"
 
+class ButtonLight(PWMLED):
+
+    def animate(self):
+        pass
+
+class Pump(DigitalOutputDevice):
+
+    disabled: bool = False
+
+    def on(self):
+        if self.disabled:
+            return
+        else:
+            super().on()
+
+@dataclass
+class DrinkSlot:
+
+    button: DrinkButton
+    light: ButtonLight
+    pump: Pump
+    id: int = -1
+    _ids = count(0)
+
+    def __post_init__(self):
+        self.id = next(self._ids)
+        print("Slot number " + str(self.id))
+        print(self)
+        self.default_action()
+
+    def default_action(self):
+        self.button.when_pressed  = lambda: self.activate()
+        self.button.when_released  = lambda: self.stop()
+
+    def activate(self):
+        self.light.on()
+        self.pump.on()
+
+    def stop(self):
+        self.light.off()
+        self.pump.off()
+
+    def cleanup(self):
+        self.button.close()
+        self.light.close()
+        self.pump.close()
+
+
 class DrinkDispenser:
+
+    slots: list[DrinkSlot] = []
+    buttons: list[DrinkButton] = []
+    lights: list[PWMLED] = []
 
     def __init__(self) -> None:
         config = configparser.ConfigParser()
         config.read("config.ini")
         print(config.sections())
-        self.b1 = DrinkButton(config["Buttons"]["B1"], pull_up=False)
-        self.b2 = DrinkButton(config["Buttons"]["B2"], pull_up=False)
-        self.b3 = DrinkButton(config["Buttons"]["B3"], pull_up=None, active_state=True)
-        self.l1 = PWMLED(config["Leds"]["L1"])
-        self.l2 = PWMLED(config["Leds"]["L2"])
-        self.l3 = PWMLED(config["Leds"]["L3"])
-        self.m1 = LED(config["Motors"]["M1"])
-        self.m2 = LED(config["Motors"]["M2"])
-        self.m3 = LED(config["Motors"]["M3"])
-        self.buttons = [self.b1, self.b2, self.b3]
-        self.leds = [self.l1, self.l2, self.l3]
-        self.motors = [self.m1, self.m2, self.m3]
+        b1 = DrinkButton(config["Buttons"]["B1"], pull_up=False)
+        b2 = DrinkButton(config["Buttons"]["B2"], pull_up=False)
+        b3 = DrinkButton(config["Buttons"]["B3"], pull_up=None, active_state=True)
+        l1 = ButtonLight(config["Leds"]["L1"])
+        l2 = ButtonLight(config["Leds"]["L2"])
+        l3 = ButtonLight(config["Leds"]["L3"])
+        m1 = Pump(config["Motors"]["M1"])
+        m2 = Pump(config["Motors"]["M2"])
+        m3 = Pump(config["Motors"]["M3"])
+        self.slots.extend(
+            [DrinkSlot(b1, l1, m1),
+             DrinkSlot(b2, l2, m2),
+             DrinkSlot(b3, l3, m3)
+             ]
+        )
+        self.buttons.extend([s.button for s in self.slots])
+        self.lights.extend([s.light for s in self.slots])
+        atexit.register(self.cleanup)
+
+    def disable_pumps(self, disabled: bool) -> None:
+        for slot in self.slots:
+            slot.pump.disabled = disabled
 
 
     def button_status(self):
         return "BUTTONS: " + ' '.join([button.status() for button in self.buttons])
 
     def cleanup(self):
-        pins = [*self.buttons, *self.leds, *self.motors]
-        for pin in pins:
-            pin.close()
+        for slot in self.slots:
+            slot.cleanup()
         subprocess.run(
             ["./gpio-reset.sh"],
             check=True
--- machine_status.py
+++ machine_status.py
@@ -3,37 +3,17 @@ import atexit
 from gpiozero import Device
 
 from drink_dispenser import DrinkDispenser
-from time import sleep
+import time
 
-
-def activate(led, motor):
-    led.on()
-    motor.on()
-
-def stop(led, motor):
-    led.off()
-    motor.off()
 
 machine = DrinkDispenser()
-
-print(Device.pin_factory)
-
-for i, button in enumerate(machine.buttons):
-    print("Number " + str(i))
-    print(button)
-    print(machine.leds[i])
-    print(machine.motors[i])
-    led = machine.leds[i]
-    motor = machine.motors[i]
-    button.when_pressed  = lambda l=led,m=motor: activate(l, m)
-    button.when_released  = lambda l=led,m=motor: stop(l, m)
-
-atexit.unregister(Device.pin_factory.close)
+machine.disable_pumps(disabled=True)
+start_time = time.time()
 
 try:
   while True:
     print(machine.button_status(), end="\r")
-    sleep(.1)
+    time.sleep(.1)
 except KeyboardInterrupt:
     print("Bar's Closed!")
     machine.cleanup()
