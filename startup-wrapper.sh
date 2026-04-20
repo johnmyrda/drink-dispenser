eadfa110b9fdb85e4927c4feed437cf20197fd61
John Myrda <hello@johnmyrda.com>
2026-04-20 09:23:58 -0500

    Ready for v1

--- startup-wrapper.sh
+++ startup-wrapper.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-program_name=basic_bar
-pgrep -a $program_name
+program_name="uv run basic_bar"
+pgrep -f -a "$program_name"
 running=$?
 
 
 if [ "$running" -eq "0" ]; then
-  echo "Program $program_name is already running. Exiting.."
+  echo "Program '$program_name' is already running. Exiting.."
   exit 0;
 else
   script_directory=$(dirname "$0")
   cd "$script_directory" || echo "Failed to cd to $script_directory" || exit 1
-  echo "Started $program_name at $(date)" >> startup.log
   nohup uv run basic_bar >/dev/null 2>&1 &
+  program_pid=$(pgrep -f "$program_name")
+  echo "Started '$program_name' [$program_pid] $(date)"
 fi;
